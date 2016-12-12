41342e9c6921c648f8b077df9dc1513d9f0c9968
michaelmernin <michael.mernin@gmail.com>
2016-12-12 13:52:19 -0600

    Jar update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,126 @@
 package com.company;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
 public class Main {
+    public static Products products;
 
     public static void main(String[] args) {
 
+        Spark.init();
 
-        
+        Spark.get(
+
+                "/api/hello",
+                ((request, response) -> {
+
+
+                    return "hello world";
+
+                })
+
+        );
+
+        Spark.get(
+
+                "/api/products",
+                ((request, response) -> {
+                    //listing of our products, which links to their description
+
+
+
+                    return "hello world";
+
+                })
+
+        );
+
+        Spark.get(
+
+                "/api/get-cart",
+                ((request, response) -> {
+                    //will be linked for all pages, will show the current attributes of the session
+                   //will have the remove, add, and change
+
+                    return "hello world";
+
+                })
+
+        );
+
+        Spark.get(
+
+                "/api/get-product",
+                ((request, response) -> {
+
+                    //switch or if statements?? on which product customer is inquiring upon (depends on session attribute?
+
+
+                    return "hello world";
+
+                })
+
+        );
+
+        Spark.get(
+
+                "/api/tax",
+                ((request, response) -> {
+
+
+                    return "hello world";
+
+                })
+
+        );
+
+        Spark.post(
+
+                "/api/add-product",
+                ((request, response) -> {
+                    //a button that allows the user to add as many to their cart as needed
+
+
+                    return "hello world";
+
+                })
+
+        );
+
+        Spark.post(
+
+                "/api/remove-product",
+                ((request, response) -> {
+
+
+                    return "hello world";
+
+                })
+
+        );
+
+        Spark.post(
+
+                "/api/change-quantity",
+                ((request, response) -> {
+
+
+                    return "hello world";
+
+                })
+
+        );
+
+
+
+
+
+
+
 
 
     }
---
+++ src/com/company/Products.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by michaelmernin on 12/12/16.
+ */
+public class Products {
+
+
+
+}
