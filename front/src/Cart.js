0e5a7c1650ab394e1323ae4b55f5a11822c0adc7
Sam Pellegrene <pellegrenesam@gmail.com>
2016-12-14 15:39:33 -0600

    added product on cart

--- front/src/Cart.js
+++ front/src/Cart.js
@@ -6,10 +6,28 @@ import axios from 'axios';
 
 
 class Cart extends Component {
-  componentDidMount (){
+  constructor(props) {
+    super(props);
+    this.state={
+      cart:[]
+    }
+  }
+
+  componentDidMount() {
+    this.getCart()
+  }
+
+  getCart() {
     axios.get('http://localhost:3000/api/products').then((response)=> {
       console.log(response.data);
+      let cart=(response.data);
+      this.setState({
+        cart
+      })
     })
+    .catch((error)=> {
+      console.log(error);
+    });
   }
 
   render() {
@@ -25,8 +43,11 @@ class Cart extends Component {
           <img className="cart-image" src={cart} alt='image' />
             <div className="opaque-box-cart">
               <ul className="product-cart">
-                <li className="product-item-cart">item1</li>
-                <li className="total-cart">total</li>
+              {this.state.cart.map((product, index) => {
+                return (
+                <li className="product-item-cart" key={product.id}>{product.name}</li>
+              )
+              })}
               </ul>
           </div>
         </div>
