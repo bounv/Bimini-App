8fdab667fdc5f2d11dd4fcbe13e1250a4ceb04f2
Sam Pellegrene <pellegrenesam@gmail.com>
2016-12-14 11:05:27 -0600

    fixed media queries

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import jpg from './HeaderNav2.jpg';
+import jpg from './HeaderNav.jpg';
 import './App.css';
 import pic from './LandingPage2.jpg';
 
@@ -7,25 +7,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <div className="App-header">
+        <div className="App-header">
           <img src={jpg} className="Nav-photo" alt="image" />
-          <p className="bimini-name">Bimini</p>
           <ul className="nav-titles">
+            <li className="bimini-name">Bimini</li>
             <li className="list-title">Products</li>
             <li className="list-title">Cart</li>
           </ul>
+        </div>
+        <h2 className="slogan-main">Water from the heavens</h2>
+        <div className="mid-section">
+          <img src={pic} className="land-photo" alt="image" />
+          <div className="opaque-box">
+            <span className="landing-page-desc">Bimini is your premiere source for artisinal waters imbued with all the finest minerals on earth. Browse our premium selection of products to find the perfect water for your needs and price range.</span>
           </div>
-          <div className="mid-header">
-            <h2 className="slogan-main">Water from the heavens</h2>
-            <div className="image-container">
-            <img src={pic} className="land-photo" alt="image" />
-              <div className="opaque-box">
-              <span className="landing-page-desc">Bimini is your premiere source for artisinal waters imbued with all the finest minerals on earth. Browse our premium selection of products to find the perfect water for your needs and price range.</span>
-              </div>
-            </div>
-          </div>
-          <p className="App-intro">
-          </p>
+        </div>
       </div>
     );
   }
