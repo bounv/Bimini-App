576da56248ea3502aa7bf988d14450027bdfc41e
Andrew S. Lentz <Andrew.S.Lentz@gmail.com>
2016-12-14 10:00:36 -0600

    basic styling of products page

--- front/src/index.js
+++ front/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ProductsPage from './ProductsPage'
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <ProductsPage />,
   document.getElementById('root')
 );
