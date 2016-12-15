6ee43a8e4dece0391ce87c89cef37338f0d5d6d6
Andrew S. Lentz <Andrew.S.Lentz@gmail.com>
2016-12-15 16:27:29 -0600

    functionality on change quantity and live updating total

--- front/src/Cart.js
+++ front/src/Cart.js
@@ -1,59 +1,72 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router';
 import './Cart.css';
 import cart from './CartPage.jpg';
 import axios from 'axios';
-let total = 0;
-
 
 class Cart extends Component {
   constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       cart: [],
-      idNumber:'',
+      idNumber: '',
+      total: 0
     }
   }
 
   componentDidMount() {
     this.getCart()
-    total = 0;
   }
 
   getCart() {
-    axios.get('http://localhost:3000/api' + '/get-cart')
-      .then((responseCart)=> {
-      axios.get('http://localhost:3000/api' + '/products')
-        .then((responseProducts)=> {
-          var cartItems =responseProducts.data.filter((v)=>{
-            return responseCart.data[''+v.id+''] > 0;
+    axios.get('http://localhost:3000/api' +
+      '/get-cart').then((responseCart) => {
+      axios.get('http://localhost:3000/api' +
+        '/products').then((responseProducts) => {
+        var cartItems = responseProducts.data.filter((v) => {
+          return responseCart.data['' + v.id + ''] > 0;
         });
-          var cartItemQuantity = cartItems.map((v)=>{
-            var newItem = v;
-          newItem.quantity = responseCart.data[''+v.id+''];
+        var cartItemQuantity = cartItems.map((v) => {
+          var newItem = v;
+          newItem.quantity = responseCart.data['' + v.id + ''];
           return newItem;
         });
-        this.setState({
-          cart: cartItemQuantity
-        })
+        this.setState({cart: cartItemQuantity})
+        this.getTotal()
       })
-    })
-    .catch((error)=> {
+    }).catch((error) => {
       console.log(error);
     });
   }
 
-  onRemoveClick(idNumber, e){
+  getTotal() {
+    let totals = this.state.cart.map((product) => {
+      return product.price * product.quantity
+    })
+    let total = totals.reduce((a, v) => {
+      return a + v;
+    }, 0)
+    this.setState({total})
+  }
+
+  onRemoveClick(idNumber, e) {
     e.preventDefault();
     console.log(idNumber);
-    axios.post('http://localhost:3000'+ '/api/remove-product?id=' + idNumber)
-      .then((response)=>{
-        // console.log(cart.id);
-        this.getCart();
-      })
-      .catch((error)=> {
-        console.log(error);
-      });
+    axios.post('http://localhost:3000' +
+      '/api/remove-product?id=' + idNumber).then((response) => {
+      // console.log(cart.id);
+      this.getCart();
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  onQuantityChange(idNumber, e) {
+    e.preventDefault()
+    axios.post('http://localhost:3000' +
+      '/api/change-quantity?id=' + idNumber + '&newQuantity=' + e.target.value).then(() => {
+      this.getCart();
+    })
   }
 
   render() {
@@ -61,36 +74,52 @@ class Cart extends Component {
       <div className="cart-page-container">
         <div className="cart-header">
           <div className='center'>
-            <h1 className="bimini-name-cart"><Link to={'/'}>Bimini</Link></h1>
+            <h1 className="bimini-name-cart">
+              <Link to={'/'}>Bimini</Link>
+            </h1>
             <h3 className="cart-name">Cart</h3>
           </div>
         </div>
         <div className="mid-section-cart">
-          <img className="cart-image" src={cart} alt='image' />
-            <div className="opaque-box-cart">
-              <ul className="product-cart">
+          <img className="cart-image" src={cart} alt='image'/>
+          <div className="opaque-box-cart">
+            <ul className="product-cart">
               {this.state.cart.map((product, index) => {
-                total = total + product.price;
                 // console.log(total);
                 let idNumber = product.id
-                console.log(product)
                 return (
-                <li className="product-item-cart" key={product.id}>
-                <controls style={{marginRight: '5rem'}}>
-                <i className="fa fa-times" aria-hidden="true" style={{color:'red', marginRight:'.5rem'}} onClick={this.onRemoveClick.bind(this, idNumber)}></i>
-                <input style={{maxWidth: '2.75rem'}} type='number' min='1' placeholder= {'qty: ' + product.quantity} />
-                </controls>
-                {product.name}: ${product.price}
-                </li>
-              )
+                  <li className="product-item-cart" key={product.id}>
+                    <controls style={{
+                      marginRight: '5rem'
+                    }}>
+                      <i className="fa fa-times" aria-hidden="true" style={{
+                        color: 'red',
+                        marginRight: '.5rem'
+                      }} onClick={this.onRemoveClick.bind(this, idNumber)}></i>
+                      <input onChange={this.onQuantityChange.bind(this, idNumber)} style={{
+                        maxWidth: '2.75rem'
+                      }} type='number' min='1' placeholder={'qty: ' + product.quantity}/>
+                    </controls>
+                    {product.name}: ${product.price}
+                  </li>
+                )
               })}
-              <hr style={{width: '100%'}}/>
-              <li className='product-item-cart' style={{textAlign: 'center'}}><span>Total:</span> <span>${total}</span></li>
-              </ul>
+              <hr style={{
+                width: '100%'
+              }}/>
+              <li className='product-item-cart' style={{
+                textAlign: 'center'
+              }}>
+                <span>Total:</span>
+                <span>${this.state.total}</span>
+              </li>
+            </ul>
           </div>
         </div>
         <div className="button-class">
-          <Link to='/checkout'><button className="button-cart">Checkout</button></Link>
+          <Link to='/checkout'>
+            <button className="button-cart">Checkout</button>
+          </Link>
         </div>
         <footr>
           <p className="phone-number">555.555.5555</p>
