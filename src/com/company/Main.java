1db870e2851efa26a88394ed1cccff20ab36f2d8
michaelmernin <michael.mernin@gmail.com>
2016-12-21 09:42:59 -0600

    Jar update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -29,6 +29,8 @@ public class Main {
             products.add(prod);
         }
 
+        Spark.port(getHerokuAssignedPort()); 
+
         Spark.init();
 
         Spark.get(
@@ -228,4 +230,14 @@ public class Main {
 
 
     }
+
+    private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+
 }
